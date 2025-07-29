b8619f38842a09c034c89a1803317a1fedc9bbe5
SaphirDeFeu <123899399+SaphirDeFeu@users.noreply.github.com>
2025-07-29 12:00:32 +0200

    Removed Temperature
    - Does not fit the character of the mod
    - Biome temperature is unstable and is not a good baseline
    - Cannot figure out another way to make Temperature work.

--- src/main/java/io/github/saphirdefeu/forgineer/init/EventRegistrar.java
+++ src/main/java/io/github/saphirdefeu/forgineer/init/EventRegistrar.java
@@ -1,11 +1,9 @@
 package io.github.saphirdefeu.forgineer.init;
 
-import io.github.saphirdefeu.forgineer.Temperature;
-
 public class EventRegistrar {
 
     public static void registerEvents() {
-        Temperature temperatureEvent = new Temperature();
+
     }
 
 }

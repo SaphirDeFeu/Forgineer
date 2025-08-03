f1c2ccc5394b3f7956032b1d9fde6d4b02bf2c5e
SaphirDeFeu <123899399+SaphirDeFeu@users.noreply.github.com>
2025-08-03 15:38:02 +0200

    Ruby
    - Added the three tiers of ruby (uncut, cut, and netherite-infused)
    - Cut Ruby provides 4 hearts of health boost
    - Ruby-Infused Netherite provides 10 hearts of health boost.

--- src/main/java/io/github/saphirdefeu/forgineer/init/ModItems.java
+++ src/main/java/io/github/saphirdefeu/forgineer/init/ModItems.java
@@ -2,6 +2,8 @@ package io.github.saphirdefeu.forgineer.init;
 
 import io.github.saphirdefeu.forgineer.Forgineer;
 import io.github.saphirdefeu.forgineer.item.MoltenMetal;
+import io.github.saphirdefeu.forgineer.item.Ruby;
+import io.github.saphirdefeu.forgineer.item.RubyNetherite;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
@@ -9,6 +11,7 @@ import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.Rarity;
 
 import java.util.function.Function;
 
@@ -29,22 +32,13 @@ public class ModItems {
                     .maxCount(1)
     );
 
-    public static final Item MOLTEN_IRON = register("molten_iron", MoltenMetal::new,
-            new Item.Settings()
-                    .fireproof()
-    );
-
-    public static final Item MOLTEN_GOLD = register("molten_gold", MoltenMetal::new,
-            new Item.Settings()
-                    .fireproof()
-    );
-
-    public static final Item MOLTEN_COPPER = register("molten_copper", MoltenMetal::new,
-            new Item.Settings()
-                    .fireproof()
-    );
+    public static final Item MOLTEN_IRON = register("molten_iron", MoltenMetal::new, MoltenMetal.settings);
+    public static final Item MOLTEN_GOLD = register("molten_gold", MoltenMetal::new, MoltenMetal.settings);
+    public static final Item MOLTEN_COPPER = register("molten_copper", MoltenMetal::new, MoltenMetal.settings);
 
     public static final Item UNCUT_RUBY = register("uncut_ruby", Item::new, new Item.Settings());
+    public static final Item RUBY = register("ruby", Ruby::new, Ruby.settings);
+    public static final Item RUBY_NETHERITE = register("ruby_netherite", RubyNetherite::new, RubyNetherite.settings);
 
     private static Item register(String name, Function<Item.Settings, Item> itemFactory, Item.Settings settings) {
         // Create item key, item instance, and register the item instance.
@@ -69,6 +63,8 @@ public class ModItems {
                     itemGroup.add(MOLTEN_COPPER);
 
                     itemGroup.add(UNCUT_RUBY);
+                    itemGroup.add(RUBY);
+                    itemGroup.add(RUBY_NETHERITE);
                 }
         );
     }
--- src/main/java/io/github/saphirdefeu/forgineer/item/Ruby.java
+++ src/main/java/io/github/saphirdefeu/forgineer/item/Ruby.java
@@ -1,31 +1,31 @@
 package io.github.saphirdefeu.forgineer.item;
 
-import io.github.saphirdefeu.forgineer.init.ModEffects;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Rarity;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.function.Consumer;
+public class Ruby extends Item {
 
-public class MoltenMetal extends Item {
+    public static final Settings settings = new Settings().rarity(Rarity.UNCOMMON);
 
-    public static final Settings settings = new Settings().fireproof();
-
-    public MoltenMetal(Settings settings) {
+    public Ruby(Settings settings) {
         super(settings);
     }
 
     @Override
     public void inventoryTick(ItemStack stack, ServerWorld world, Entity entity, @Nullable EquipmentSlot slot) {
         if(entity instanceof LivingEntity) {
-            var hot_hand_instance = new StatusEffectInstance(ModEffects.HOT_HAND, 2, 0, false, true, true);
-            ((LivingEntity) entity).addStatusEffect(hot_hand_instance);
+            StatusEffectInstance instance = new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 2, 1, false, false, true);
+            ((LivingEntity) entity).addStatusEffect(instance);
         }
+
         super.inventoryTick(stack, world, entity, slot);
     }
 }

41e97d38724ad817f7575ee2f46d69aa8c392f2b
SaphirDeFeu <123899399+SaphirDeFeu@users.noreply.github.com>
2025-08-21 13:46:19 +0200

    added sound feedback to gemstone detector

--- src/main/java/io/github/saphirdefeu/forgineer/item/GemstoneDetector.java
+++ src/main/java/io/github/saphirdefeu/forgineer/item/GemstoneDetector.java
@@ -3,6 +3,8 @@ package io.github.saphirdefeu.forgineer.item;
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Rarity;
@@ -10,7 +12,6 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class GemstoneDetector extends Item {
@@ -49,7 +50,8 @@ public class GemstoneDetector extends Item {
         }
 
         Vec3d exactPosition = user.getPos();
-        ArrayList<Double> distances = new ArrayList<>();
+        double minDist = RADIUS + 10;
+        BlockPos minDistBlockPos = new BlockPos(0, 0, 0);
         for(BlockPos blockPos : ores) {
             double dX = exactPosition.getX() - blockPos.getX();
             double dY = exactPosition.getY() - blockPos.getY();
@@ -57,20 +59,19 @@ public class GemstoneDetector extends Item {
             // Expensive & Scary !!
             double dist = Math.sqrt(dX * dX + dY * dY + dZ * dZ);
             if(dist >= RADIUS) continue;
+            if(dist >= minDist) continue;
 
-            distances.add(dist);
+            minDist = dist;
+            minDistBlockPos = blockPos;
         }
 
-        if(distances.isEmpty()) {
+        if(minDist == RADIUS + 10) {
             user.getStackInHand(hand).setDamage(user.getStackInHand(hand).getMaxDamage() - 1);
             return ActionResult.PASS;
         }
 
-        distances.sort(Double::compareTo);
-
-        double dist = distances.getFirst();
-
-        user.getStackInHand(hand).setDamage((int) (dist * 10));
+        user.getStackInHand(hand).setDamage((int) (minDist * 10));
+        world.playSound(null, minDistBlockPos, SoundEvents.BLOCK_NOTE_BLOCK_CHIME.value(), SoundCategory.BLOCKS, RADIUS, 0f);
 
         return ActionResult.SUCCESS;
     }

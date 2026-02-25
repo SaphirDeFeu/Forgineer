df02356ac825a6be5dff30d539c50a0e3a442087
SaphirDeFeu <saphirdefeu@proton.me>
2026-02-25 11:32:53 +0100

    fix gem effects disappearing on death
    courtesy of rjh-2087

--- src/main/java/io/github/saphirdefeu/forgineer/event/PlayConnectionListener.java
+++ src/main/java/io/github/saphirdefeu/forgineer/event/PlayConnectionListener.java
@@ -1,25 +1,12 @@
 package io.github.saphirdefeu.forgineer.event;
 
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.Multimap;
-import io.github.saphirdefeu.forgineer.Forgineer;
-import io.github.saphirdefeu.forgineer.state.PlayerData;
+import io.github.saphirdefeu.forgineer.item.Gemstone;
 import io.github.saphirdefeu.forgineer.state.StateManager;
 import net.fabricmc.fabric.api.networking.v1.PacketSender;
-import net.minecraft.entity.attribute.EntityAttribute;
-import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.entry.RegistryEntry;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
-import net.minecraft.util.Identifier;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
-// TODO: move logic to apply attribute modifiers in onJoin() to a separate method to be called separately for better modularity
-// TODO: in case something goes wrong (which it always does)
 public class PlayConnectionListener {
     public static void onJoin(
             ServerPlayNetworkHandler serverPlayNetworkHandler,
@@ -27,34 +14,7 @@ public class PlayConnectionListener {
             MinecraftServer minecraftServer
     ) {
         StateManager state = StateManager.getServerState(minecraftServer);
-
-        // Find the player who just joined and if their UUID is contained in the list of attribute modifiers on the server state
         PlayerEntity player = serverPlayNetworkHandler.getPlayer();
-        HashMap<String, PlayerData> players = state.getPlayers();
-        String uuid = player.getUuidAsString();
-        if(!players.containsKey(uuid)) {
-            return;
-        }
-
-        // Make a list of this player's modifications to apply them after
-        PlayerData playerData = players.get(uuid);
-        ArrayList<String> attributeIdentifiers = playerData.getAttributeIdentifiers();
-        ArrayList<Double> attributeValues = playerData.getAttributeValues();
-
-        for(int i = 0; i < attributeValues.size(); i++) {
-            // For each instance of a modified attribute, apply it to the player
-            String attributeIdentifier = attributeIdentifiers.get(i);
-            double attributeValue = attributeValues.get(i);
-
-            EntityAttributeModifier entityAttributeModifier = new EntityAttributeModifier(
-                    Identifier.of(Forgineer.MOD_ID, "gemstone"), attributeValue, EntityAttributeModifier.Operation.ADD_VALUE
-            );
-
-            Multimap<RegistryEntry<EntityAttribute>, EntityAttributeModifier> modifierMultimap = ArrayListMultimap.create();
-            // Convert Attribute ID to RegistryEntry
-            RegistryEntry<EntityAttribute> attributeEntry = Registries.ATTRIBUTE.getEntry(Registries.ATTRIBUTE.get(Identifier.of(attributeIdentifier)));
-            modifierMultimap.put(attributeEntry, entityAttributeModifier);
-            player.getAttributes().addTemporaryModifiers(modifierMultimap);
-        }
+        Gemstone.setPlayerAttributeModifiers(player, state);
     }
 }
--- src/main/java/io/github/saphirdefeu/forgineer/init/ForgineerEventRegistrar.java
+++ src/main/java/io/github/saphirdefeu/forgineer/init/ForgineerEventRegistrar.java
@@ -2,6 +2,8 @@ package io.github.saphirdefeu.forgineer.init;
 
 import io.github.saphirdefeu.forgineer.event.AttackBlockListener;
 import io.github.saphirdefeu.forgineer.event.PlayConnectionListener;
+import io.github.saphirdefeu.forgineer.event.ServerPlayerListener;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
 
@@ -12,6 +14,8 @@ public class ForgineerEventRegistrar {
         ServerPlayConnectionEvents.JOIN.register(PlayConnectionListener::onJoin);
 
         AttackBlockCallback.EVENT.register(AttackBlockListener::onAttackBlock);
+
+        ServerPlayerEvents.AFTER_RESPAWN.register(ServerPlayerListener::afterRespawn);
 
     }
 
